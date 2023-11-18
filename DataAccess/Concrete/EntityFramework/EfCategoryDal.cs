f1d751b755ca140e0bff02e8398b859e2283a86b
Okan YILMAZ <okan_yilmaz28@hotmail.com>
2023-11-18 20:17:24 +0300

    The Entity Framework classes have been defined.

---
+++ DataAccess/Concrete/EntityFramework/EfCategoryDal.cs
@@ -0,0 +1,15 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract.EntityFramework;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfCategoryDal : EfEntityRepositoryBase<Category, FlickVerseContext>, ICategoryDal
+    {
+    }
+}
